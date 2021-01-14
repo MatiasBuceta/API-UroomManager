302802fba5d2221de42abe1f4c23f1fe552c7a77
Matias <46636804+MatiasBuceta@users.noreply.github.com>
2021-01-13 22:51:30 -0300

    Test inicial

--- UroomManager/UroomManager/Controllers/WeatherForecastController.cs
+++ UroomManager/UroomManager/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace UroomManager.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Tinchito", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
