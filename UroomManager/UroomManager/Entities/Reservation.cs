1a3367502efbe4cd4d860a5ba278ddc2d199099c
Matias <46636804+MatiasBuceta@users.noreply.github.com>
2021-01-16 05:06:51 -0300

    Reservation Class Completed

--- UroomManager/UroomManager/Entities/Reservation.cs
+++ UroomManager/UroomManager/Entities/Reservation.cs
@@ -9,6 +9,13 @@ namespace UroomManager.Entities
     {
         public int Id { get; set; }
         public int RoomId { get; set; }
-        public TimeSpan ReservationTime { get; set; }
+        public int Assistants { get; set; }
+        public int Projector { get; set; }
+        public int Blackboard { get; set; }
+        public int Internet { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public string HoraInicioReserva { get; set; }
+        public string HoraFinalReserva { get; set; }
+
     }
 }
